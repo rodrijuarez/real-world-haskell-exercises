50be750e920ad20940b96348c004f6b423ceeb44
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-08-12 21:53:14 +0200

    Right is right

--- ch04/IntParse.hs
+++ ch04/IntParse.hs
@@ -1,4 +1,20 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, isDigit)
+import Data.Either (Either, isRight)
 
-asInt :: String -> Int
-asInt xs = foldl (\acc x -> acc * 10 + digitToInt x) 0 xs
+asInt :: String -> Either String Int
+asInt ('-':xs) =
+  let xs' = asInt xs
+  in case xs' of
+       Right xs'' -> Right (negate (xs''))
+       Left xs'' -> Left xs''
+asInt xs =
+  foldl
+    (\acc x ->
+       case acc of
+         Right acc' ->
+           if isDigit (x)
+             then Right (acc' * 10 + (digitToInt x))
+             else Left "non-digit"
+         Left acc' -> Left acc')
+    (Right 0)
+    xs
