5135f3c0d616146c7d5dd5594ac2219bcbd08c49
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-08-13 21:07:03 +0200

    Take while recursive and with fold

--- ch04/ch04.exercises.hs
+++ ch04/ch04.exercises.hs
@@ -1,2 +1,23 @@
 myConcat :: [[a]] -> [a]
 myConcat xs = foldr (\acc x -> acc ++ x) [] xs
+
+recursiveTakeWhile :: (a -> Bool) -> [a] -> [a]
+recursiveTakeWhile cond [] = []
+recursiveTakeWhile cond [x] =
+  if not (cond (x))
+    then []
+    else [x]
+recursiveTakeWhile cond (x:xs) =
+  if not (cond (x))
+    then []
+    else [x] ++ (recursiveTakeWhile cond xs)
+
+myTakeWhile :: (a -> Bool) -> [a] -> [a]
+myTakeWhile cond xs =
+  foldr
+    (\x y ->
+       if cond (x)
+         then x : y
+         else [])
+    []
+    xs
