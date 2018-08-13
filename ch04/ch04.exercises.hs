4665f6382ee34681c410bcdab0838edc1c2ac2f1
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-08-13 21:45:59 +0200

    :oooo

--- ch04/ch04.exercises.hs
+++ ch04/ch04.exercises.hs
@@ -3,14 +3,10 @@ myConcat xs = foldr (\acc x -> acc ++ x) [] xs
 
 recursiveTakeWhile :: (a -> Bool) -> [a] -> [a]
 recursiveTakeWhile cond [] = []
-recursiveTakeWhile cond [x] =
-  if not (cond (x))
-    then []
-    else [x]
 recursiveTakeWhile cond (x:xs) =
   if not (cond (x))
     then []
-    else [x] ++ (recursiveTakeWhile cond xs)
+    else x : (recursiveTakeWhile cond xs)
 
 myTakeWhile :: (a -> Bool) -> [a] -> [a]
 myTakeWhile cond xs =
@@ -19,5 +15,18 @@ myTakeWhile cond xs =
        if cond (x)
          then x : y
          else [])
+    []
+    xs
+
+myGroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+myGroupBy cond xs =
+  foldr
+    (\x acc ->
+       case acc of
+         [] -> [[x]]
+         ((y:yGroup):rest) ->
+           if (cond x y)
+             then [x : y : yGroup] ++ rest
+             else [x] : [y : yGroup] ++ rest)
     []
     xs
