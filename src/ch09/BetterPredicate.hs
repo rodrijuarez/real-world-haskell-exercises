a74d56f52ebf5435d91bd22d614f2c9c7a857073
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-08-26 09:59:09 +0200

    Some clean up for keeping sanity

--- src/ch09/BetterPredicate.hs
+++ src/ch09/BetterPredicate.hs
@@ -10,29 +10,14 @@ import System.IO (IOMode(..), hClose, hFileSize, openFile)
 
 import RecursiveContents (getRecursiveContents)
 
-type Predicate = FilePath -> Permissions -> Maybe Integer -> UTCTime -> Bool
-
 getFileSize :: FilePath -> IO (Maybe Integer)
 getFileSize path =
   handle ((\_ -> return Nothing) :: IOError -> IO (Maybe Integer)) $
   bracket (openFile path ReadMode) hClose $ \h -> do
     size <- hFileSize h
     return (Just size)
-  --h <- openFile path ReadMode
-  --size <- hFileSize h
-  --hClose h
-  --return size
-  --handle (\_ -> return Nothing) $ do
-    --h <- openFile path ReadMode
-    --size <- hFileSize h
-    --hClose h
-    --return (Just size)
 
---simpleFileSize :: FilePath -> IO Integer
---simpleFileSize path = do
---saferFileSize :: FilePath -> IO (Maybe Integer)
---saferFileSize path =
-betterFind :: Predicate -> FilePath -> IO [FilePath]
+betterFind :: InfoP Bool -> FilePath -> IO [FilePath]
 betterFind p path = getRecursiveContents path >>= filterM check
   where
     check name = do
@@ -40,3 +25,52 @@ betterFind p path = getRecursiveContents path >>= filterM check
       size <- getFileSize name
       modified <- getModificationTime name
       return (p name perms size modified)
+
+myTest4 = liftPath takeExtension ==? ".cpp" &&? sizeP >? 131072
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath f w _ _ _ = f w
+
+type InfoP a = FilePath -> Permissions -> Maybe Integer -> UTCTime -> a
+
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
+
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing _ = -1
+
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k = \w x y z -> f w x y z == k
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+
+lesserP = liftP (<)
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+andP = liftP2 (&&)
+
+orP = liftP2 (||)
+
+constP :: a -> InfoP a
+constP k _ _ _ _ = k
+
+infix 4 ==?
+
+infixr 3 &&?
+
+infix 4 >?
+
+(==?) = equalP
+
+(&&?) = andP
+
+(>?) = greaterP
+
+liftP' q f k w x y z = f w x y z `q` constP k w x y z
